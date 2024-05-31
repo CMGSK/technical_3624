de20d5ae1859e6557def1e6f14fd3e8f7603da44
CMGSK@MSI <carlosmgov7k@gmail.com>
2024-05-31 22:29:49 +0200

    first commit

---
+++ Technical/Point3D.cs
@@ -0,0 +1,58 @@
+﻿namespace Technical;
+
+public class Point3D
+{
+    //Define each axis and an optional identifier
+    public string Id { get; set; } = string.Empty;
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+
+    //Constructors with/without identifier
+    public Point3D(int x, int y, int z, string id){
+        X = x;
+        Y = y;
+        Z = z;
+        Id = id;
+    }
+
+    public Point3D(int x, int y, int z){
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}(X:{X}, Y:{Y}, Z:{Z})";
+    }
+
+    //3D euclidean distance
+    public float DistanceToAsFloat(Point3D target)
+    {
+        return (float) Math.Sqrt(Math.Pow(X - target.X, 2) + Math.Pow(Y - target.Y, 2) + Math.Pow(Z - target.Z, 2));
+    }
+
+    //Just in case you wanted the coordinate distance for each axis. 
+    // public Point3D DistanceToAsCoordinates(Point3D target)
+    // {
+    //     return new Point3D(X-target.X, Y-target.Y, Z-target.Z);
+    // }
+
+    //Move to the specified coordinates without reinstanciating the object
+    public void MoveTo(int x, int y, int z)
+    {
+        X = x; 
+        Y = y; 
+        Z = z; 
+    }
+
+    //Just another way I thought useful to "move" relative to its position and not bound by mandatory arguments
+    public void DisplaceOn(int x = 0, int y = 0, int z = 0)
+    {
+        X += x;
+        Y += y;
+        Z += z;
+    }
+}
---
+++ Technical/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Technical;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Point3D> coordinates = File.ReadAllLines("./Input3DPoints.txt") //Read the file as array of lines
+            .Select(line => $"{line.Split('=')[0]}," + line.Split('=')[1].Trim("( )".ToCharArray())) //Format each one to name,x,y,z
+            .Select(coords => coords.Split(',').ToArray()) //Split it into an array using the comma
+            .Select(nxyz => new Point3D(int.Parse(nxyz[1]), int.Parse(nxyz[2]), int.Parse(nxyz[3]), nxyz[0])) //Build the object parsing xyz into int
+            .ToList(); //return it as a list to match coordinates variable
+
+        Console.WriteLine("\n\tThe points are:");
+        foreach (Point3D p in coordinates) 
+            Console.WriteLine(p);
+
+        Console.WriteLine("\n\tPoint 1 is:");
+        foreach (Point3D p in coordinates.Skip(1)) //Skip the index 0 since is the one we are comparing
+            Console.WriteLine($"At {coordinates[0].DistanceToAsFloat(p):00.000} units from {p}");
+
+        Console.WriteLine($"\nMoving {coordinates[0]}...");
+        coordinates[0].MoveTo(7,3,9);
+        Console.WriteLine($"\tNow {coordinates[0]}");
+
+        Console.WriteLine($"\nDisplacing {coordinates[0]} +5 on Y axis...");
+        coordinates[0].DisplaceOn(y: 5);
+        Console.WriteLine($"\tNow {coordinates[0]}");
+
+
+    }
+
+}
